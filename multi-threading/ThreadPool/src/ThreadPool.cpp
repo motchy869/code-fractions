0eb0242d73fa4c84169a1f8ac7839f16a6e07a62
motchy869 <motchy869@gmail.com>
2021-08-01 22:29:05 +0900

    fix: too short chance to threads to be born

--- multi-threading/ThreadPool/src/ThreadPool.cpp
+++ multi-threading/ThreadPool/src/ThreadPool.cpp
@@ -1,8 +1,11 @@
 #include "../include/ThreadPool.hpp"
 
-static void thread_runExecutable(ThreadInfo threadInfo, std::reference_wrapper<MultiThreadQueue<std::shared_ptr<Executable>>> ref_queue) {
-    std::this_thread::yield();
-    std::this_thread::sleep_for(std::chrono::microseconds(1)); // Let the constructor create other threads
+static void thread_runExecutable(std::reference_wrapper<std::mutex> ref_threadsCreationMutex, ThreadInfo threadInfo, std::reference_wrapper<MultiThreadQueue<std::shared_ptr<Executable>>> ref_queue) {
+    /* Wait until all the other threads be created, otherwise the constructor is blocked and cannot create other threads. */
+    std::unique_lock<std::mutex> lock(ref_threadsCreationMutex.get());
+    lock.unlock();
+    std::this_thread::sleep_for(std::chrono::microseconds(100));
+
     MultiThreadQueue<std::shared_ptr<Executable>> &queue = ref_queue.get();
     std::shared_ptr<Executable> exe;
     while (queue.pop(exe)) {
@@ -11,8 +14,9 @@ static void thread_runExecutable(ThreadInfo threadInfo, std::reference_wrapper<M
 }
 
 ThreadPool::ThreadPool(unsigned int numThreads, unsigned int queueDepth) : m_numThreads(numThreads), m_threads(numThreads), m_queue(queueDepth) {
+    std::lock_guard<std::mutex> lock(m_threadsMtx);
     for (unsigned int i=0; i<m_numThreads; ++i) {
-        m_threads.push_back(std::thread(thread_runExecutable, (ThreadInfo){.threadId=i}, std::ref(m_queue)));
+        m_threads.push_back(std::thread(thread_runExecutable, std::ref(m_threadsMtx), (ThreadInfo){.threadId=i}, std::ref(m_queue)));
     }
 }
 
