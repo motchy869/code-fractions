763624e5f0242e4a9f7c2afc05709b78b0971da3
motchy <motchy869@gmail.com>
2024-04-29 19:27:40 +0900

    feat: (WIP) my_mod_csr

--- FPGA/sim/PeakRDL_trial/build-tools/csr/common.sh
+++ FPGA/sim/PeakRDL_trial/build-tools/csr/common.sh
@@ -6,9 +6,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SRC_DIR=$SCRIPT_DIR/../../src
 RDL_DIR=$SRC_DIR/rdl
 RTL_DIR=$SRC_DIR/rtl/csr
+C_HEADER_DIR=$SRC_DIR/c_cpp/csr
 DOC_DIR=$SCRIPT_DIR/../../build-out/doc/addr_map
 RDL_FILE_PATH=$RDL_DIR/my_mod_csr.rdl
-CPUIF_NAME=axi4-lite
+
+# flat type is more convenient than non-flat when designers have their own interface written in SV and want to use it.
+#CPUIF_NAME=axi4-lite
+CPUIF_NAME=axi4-lite-flat
 
 # SystemVerilog struct type naming style.
 # behaviour of "lexical" is described in "Generated type naming rules" in "SystemRDL 2.0 Register Description Language"
--- FPGA/sim/PeakRDL_trial/build-tools/csr/gen_sv_code.sh
+++ FPGA/sim/PeakRDL_trial/build-tools/csr/gen_sv_code.sh
@@ -5,3 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/common.sh
 
 peakrdl regblock $RDL_FILE_PATH -o $RTL_DIR --cpuif $CPUIF_NAME --type-style=$TYPE_STYLE
+
+RDL_FILE_NAME=$(basename $RDL_FILE_PATH)
+RTL_FILE_NAME=${RDL_FILE_NAME%.*}.sv
+RTL_FILE_PATH=$RTL_DIR/$RTL_FILE_NAME
+
+python3 $SCRIPT_DIR/refactor_sv_code.py $RTL_FILE_PATH
