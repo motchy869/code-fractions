2ced45c660aefa93de21d9ec2a9bf008dc1c7301
motchy <motchy869@gmail.com>
2024-04-30 16:16:20 +0900

    refactor: explicit data type

--- FPGA/synth/ram/src/ram_sp_wf.svh
+++ FPGA/synth/ram/src/ram_sp_wf.svh
@@ -6,13 +6,13 @@ module ram_sp_wf #(
     parameter int DEPTH = 8, //! depth of RAM, **must be power of 2**
     parameter bit USE_OUTPUT_REG = 0 //! output register option, 0/1: not use/ use
 )(
-    input wire i_clk, //! clock signal
-    input wire i_sync_rst, //! synchronous reset signal
-    input wire i_we, //! write enable signal
-    input wire [$clog2(DEPTH)-1:0] i_word_addr, //! word address
-    input wire [WORD_BIT_WIDTH-1:0] i_data, //! input data
-    input wire [WORD_BIT_WIDTH/8-1:0] i_byte_en, //! byte enable signal
-    output wire [WORD_BIT_WIDTH-1:0] o_data //! output data
+    input wire logic i_clk, //! clock signal
+    input wire logic i_sync_rst, //! synchronous reset signal
+    input wire logic i_we, //! write enable signal
+    input wire logic [$clog2(DEPTH)-1:0] i_word_addr, //! word address
+    input wire logic [WORD_BIT_WIDTH-1:0] i_data, //! input data
+    input wire logic [WORD_BIT_WIDTH/8-1:0] i_wr_byte_en, //! write byte enable signal
+    output wire logic [WORD_BIT_WIDTH-1:0] o_data //! output data
 );
 
 `endif
