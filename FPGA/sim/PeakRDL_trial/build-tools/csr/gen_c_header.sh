81f2695880469599cafc6969c3d2b2794adea6ca
motchy <motchy869@gmail.com>
2024-04-30 17:07:55 +0900

    feat: refactor C header generated by PeakRDL

--- FPGA/sim/PeakRDL_trial/build-tools/csr/gen_c_header.sh
+++ FPGA/sim/PeakRDL_trial/build-tools/csr/gen_c_header.sh
@@ -10,3 +10,5 @@ C_HEADER_FILE_NAME=$(basename ${RDL_FILE_PATH%.*}.h)
 C_HEADER_FILE_PATH=$C_HEADER_DIR/$C_HEADER_FILE_NAME
 
 peakrdl c-header $RDL_FILE_PATH --peakrdl-cfg $CFG_FILE_PATH --type-style=$TYPE_STYLE -o $C_HEADER_FILE_PATH
+
+python3 $SCRIPT_DIR/refactor_c_header.py $C_HEADER_FILE_PATH
