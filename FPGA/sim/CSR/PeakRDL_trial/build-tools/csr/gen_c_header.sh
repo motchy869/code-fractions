b7ef1d1c70ca8e0b8d4dd3b1f549c245c0274344
motchy <motchy869@gmail.com>
2024-05-08 19:00:28 +0900

    build: make some variables `readonly`

--- FPGA/sim/CSR/PeakRDL_trial/build-tools/csr/gen_c_header.sh
+++ FPGA/sim/CSR/PeakRDL_trial/build-tools/csr/gen_c_header.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/common.sh
 
 # Assumes that the base name of configuration TOML file and output C header file are equal to that of RDL file.
-CFG_FILE_PATH=${RDL_FILE_PATH%.*}.toml
-C_HEADER_FILE_NAME=$(basename ${RDL_FILE_PATH%.*}.h)
-C_HEADER_FILE_PATH=$C_HEADER_DIR/$C_HEADER_FILE_NAME
+readonly CFG_FILE_PATH=${RDL_FILE_PATH%.*}.toml
+readonly C_HEADER_FILE_NAME=$(basename ${RDL_FILE_PATH%.*}.h)
+readonly C_HEADER_FILE_PATH=$C_HEADER_DIR/$C_HEADER_FILE_NAME
 
 mkdir -p $C_HEADER_DIR
 peakrdl c-header $RDL_FILE_PATH --peakrdl-cfg $CFG_FILE_PATH --type-style $TYPE_STYLE -o $C_HEADER_FILE_PATH
