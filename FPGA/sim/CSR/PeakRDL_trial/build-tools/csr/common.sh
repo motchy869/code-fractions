980a08c16779070c422d4dd994763a8ba701c7d1
motchy <motchy869@gmail.com>
2024-05-22 01:07:23 +0900

    build: treat undefined variable reference as error

--- FPGA/sim/CSR/PeakRDL_trial/build-tools/csr/common.sh
+++ FPGA/sim/CSR/PeakRDL_trial/build-tools/csr/common.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -u
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # You should change the following two variables to match your project.
--- FPGA/sim/CSR/PeakRDL_trial/build-tools/csr/view_doc.sh
+++ FPGA/sim/CSR/PeakRDL_trial/build-tools/csr/view_doc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -u
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 python3 -m http.server -d $DOC_DIR
