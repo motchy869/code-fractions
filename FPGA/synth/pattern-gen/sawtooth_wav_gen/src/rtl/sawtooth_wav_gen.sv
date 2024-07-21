16ab01f07ae662d3ef1af3e77960d4cc299b727a
motchy <motchy869@gmail.com>
2024-07-21 11:05:41 +0900

    docs: refine comments

--- FPGA/synth/pattern-gen/sawtooth_wav_gen/src/rtl/sawtooth_wav_gen.sv
+++ FPGA/synth/pattern-gen/sawtooth_wav_gen/src/rtl/sawtooth_wav_gen.sv
@@ -6,7 +6,8 @@
 
 `default_nettype none
 
-//! Creates a sawtooth waveform.
+//! Creates a sawtooth waveform with SSR >= 1.
+//! The period can be specified in fixed-point number format.
 //!
 //! Features not implemented yet:
 //! - convergence rounding of output values
@@ -19,10 +20,10 @@ module sawtooth_wav_gen #(
     input wire logic i_clk, //! input clock
     input wire logic i_sync_rst, //! input reset synchronous to the input clock
     //! @virtualbus cfg_if @dir in configuration interface
-    input wire logic signed [BIT_WIDTH__OUTPUT-1:0] i_start_val, //! start value
-    input wire logic signed [BIT_WIDTH__OUTPUT-1:0] i_end_val, //! end value
+    input wire logic signed [BIT_WIDTH__OUTPUT-1:0] i_start_val, //! the value at the start of the period (inclusive)
+    input wire logic signed [BIT_WIDTH__OUTPUT-1:0] i_end_val, //! the value at the end of the period (**exclusive**)
     input wire logic [BIT_WIDTH__INT_PART__PERIOD-1:0] i_int_part__period, //! Integer part of the period (relative to sample period).
-    input wire logic [BIT_WIDTH__FRAC_PART__PERIOD-1:0] i_frac_part__period, //! Fractional part of the period (relative to sample period). The resulting physical period is ```(i_int_part__period + i_frac_part__period/(1<<B_F))*T_clk/SSR``` where ```T_clk``` is the period in second of the input clock and ```B_F``` is ```BIT_WIDTH__FRAC_PART__PERIOD```.
+    input wire logic [BIT_WIDTH__FRAC_PART__PERIOD-1:0] i_frac_part__period, //! Fractional part of the period. The resulting physical period is ```(i_int_part__period + i_frac_part__period/(1<<B_F))*T_clk/SSR``` where ```T_clk``` is the period in second of the input clock and ```B_F``` is ```BIT_WIDTH__FRAC_PART__PERIOD```.
     //! @end
     //! @virtualbus ds_side_if @dir out downstream side interface
     //! input ready signal which indicates that the downstream side is ready to accept a new chunk
