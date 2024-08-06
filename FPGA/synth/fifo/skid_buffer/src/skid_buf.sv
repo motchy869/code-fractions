10b55bc9207f5b229f9e8dde32b30810c24e1ae0
motchy <motchy869@gmail.com>
2024-08-06 16:30:04 +0900

    feat: workaround for Quartus Prime Lite

--- FPGA/synth/fifo/skid_buffer/src/skid_buf.sv
+++ FPGA/synth/fifo/skid_buffer/src/skid_buf.sv
@@ -6,21 +6,34 @@
 
 //! simple skid buffer which can be used to cut timing arc
 module skid_buf #(
-    parameter type T = logic //! data type
+    // Quartus Prime Lite 23.1std.1 doesn't support type parameter.
+    `ifdef QUARTUS_PRIME_LITE // This macro should be set MANUALLY in the project settings
+        parameter int unsigned BIT_WIDTH_DATA = 8 //! bit width of the data
+    `else
+        parameter type T = logic //! data type
+    `endif
 )(
     input wire logic i_clk, //! clock signal
     input wire logic i_sync_rst, //! synchronous reset signal
 
     //! @virtualbus us_side_if @dir in upstream side interface
     input wire logic i_us_valid, //! valid signal from upstream
-    input wire T i_us_data, //! data from upstream
+    `ifdef QUARTUS_PRIME_LITE
+        input wire logic [BIT_WIDTH_DATA-1:0] i_us_data, //! data from upstream
+    `else
+        input wire T i_us_data, //! data from upstream
+    `endif
     output wire logic o_us_ready, //! A ready signal to upstream. **masked by** `i_sync_rst` (to avoid losing data at transition to reset state).
     //! @end
 
     //! @virtualbus ds_side_if @dir out downstream side interface
     //! ready signal from downstream
     input wire logic i_ds_ready,
-    output wire T o_ds_data, //! data to downstream
+    `ifdef QUARTUS_PRIME_LITE
+        output wire logic [BIT_WIDTH_DATA-1:0] o_ds_data, //! data to downstream
+    `else
+        output wire T o_ds_data, //! data to downstream
+    `endif
     output wire logic o_ds_valid //! valid signal to downstream
     //! @end
 );
@@ -31,7 +44,11 @@ module skid_buf #(
 // --------------------
 
 // ---------- internal signal and storage ----------
-//! FIFO read and write pointer type
+`ifdef QUARTUS_PRIME_LITE
+    typedef logic [BIT_WIDTH_DATA-1:0] T;
+`endif
+
+// FIFO read and write pointer type
 typedef struct packed {
     logic idx; //! buffer index
     logic phase; //! The buffer phase. Begins at 0 and toggles between 0 and 1 every time the index wraps around. This is utilized for distinguishing between full and empty conditions.
