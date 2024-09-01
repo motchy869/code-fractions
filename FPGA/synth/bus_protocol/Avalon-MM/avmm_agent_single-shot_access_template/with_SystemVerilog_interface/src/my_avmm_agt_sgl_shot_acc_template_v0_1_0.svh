7eb20ea7af89c8b78cea4994b713fffd94f8ef08
motchy <motchy869@gmail.com>
2024-09-01 19:38:38 +0900

    chore: explicit data type

--- FPGA/synth/bus_protocol/Avalon-MM/avmm_agent_single-shot_access_template/with_SystemVerilog_interface/src/my_avmm_agt_sgl_shot_acc_template_v0_1_0.svh
+++ FPGA/synth/bus_protocol/Avalon-MM/avmm_agent_single-shot_access_template/with_SystemVerilog_interface/src/my_avmm_agt_sgl_shot_acc_template_v0_1_0.svh
@@ -13,8 +13,8 @@ extern module my_avmm_agt_sgl_shot_acc_template_v0_1_0 #(
     parameter int AVMM_ADDR_BIT_WIDTH = 2, //! Bit width of Avalon-MM address bus. Typically log2(number of registers). Note that in default Avalon uses **byte** addressing in hosts and **word** addressing in agents.
     parameter int AVMM_DATA_BIT_WIDTH = 32 //! bit width of Avalon-MM data bus
 )(
-    input wire i_clk, //! clock signal
-    input wire i_sync_rst, //! reset signal synchronous to clock
+    input wire logic i_clk, //! clock signal
+    input wire logic i_sync_rst, //! reset signal synchronous to clock
     avmm_if_v0_1_0.agt_pt if_agt_avmm //! Avalon-MM agent interface
 );
 
