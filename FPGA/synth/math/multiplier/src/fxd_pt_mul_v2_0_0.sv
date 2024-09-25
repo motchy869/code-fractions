ea2621e03b3dc5b54b7b28e4db431dd5c655a148
motchy <motchy869@gmail.com>
2024-09-25 20:19:04 +0900

    feat: fxd_pt_mul_v2_0_0

---
+++ FPGA/synth/math/multiplier/src/fxd_pt_mul_v2_0_0.sv
@@ -0,0 +1,178 @@
+// Verible directive
+// verilog_lint: waive-start parameter-name-style
+// verilog_lint: waive-start line-length
+
+`include "round_hf2evn_v1_0_0.svh"
+
+`default_nettype none
+
+//! A fully-parametrized fixed-point number multiplier with optional rounding half to even.
+//! Total cycle latency is ```MULT_INPUT_STG_PIPELINE_DEPTH + MULT_OUTPUT_STG_PIPELINE_DEPTH + ENABLE_ROUNDING_HALF_TO_EVEN```.
+//!
+//! When ```ENABLE_ROUNDING_HALF_TO_EVEN``` is set to ```1'b0```, the output is simply the bit slice of the product of the inputs.
+//! When ```ENABLE_ROUNDING_HALF_TO_EVEN``` is set to ```1'b1```, ```BIT_SLICE_OFFSET_OUT``` must be greater than 0.
+//!
+//! It depends on the synthesis tool whether DSP blocks are used or not.
+//! ## changelog
+//! ### [2.0.0] - 2024-09-24
+//! - change:
+//!   1. Renamed module to ```fxd_pt_mul_v2_0_0```.
+//!   2. Default value of output stage pipeline depth of the multiplier is now 1.
+//! - add: input stage pipeline
+//! ### [1.0.1] - 2024-09-20
+//! - fix: slipping valid signal delay line
+//! ### [1.0.0] - 2024-08-01
+//! - initial release
+module fxd_pt_mul_v2_0_0 #(
+    parameter int unsigned BIT_WIDTH_IN_A = 16, //! bit width of the input a
+    parameter int unsigned BIT_WIDTH_IN_B = 16, //! bit width of the input b
+    parameter int unsigned BIT_WIDTH_OUT = 16, //! bit width of the output
+    parameter int unsigned BIT_SLICE_OFFSET_OUT = 0, //! Bit slice offset of the output. When ```BIT_SLICE_OFFSET_OUT``` > 0, (ab)[```BIT_SLICE_OFFSET_OUT```-1:0] is treated as fractional part. The fractional part is simply truncated or rounded (see: ```ENABLE_ROUNDING_HALF_TO_EVEN```).
+    parameter int unsigned MULT_INPUT_STG_PIPELINE_DEPTH = 1, //! input stage pipeline depth of the multiplier, must be greater than 0
+    parameter int unsigned MULT_OUTPUT_STG_PIPELINE_DEPTH = 1, //! output stage pipeline depth of the multiplier, must be greater than 0
+    parameter bit ENABLE_ROUNDING_HALF_TO_EVEN = 1'b1 //! enable rounding half to even
+)(
+    input wire logic i_clk, //! input clock
+    input wire logic i_sync_rst, //! input reset synchronous to the input clock
+    //! @virtualbus us_side_if @dir in upstream side interface
+    output wire logic o_ready, //! ready signal to upstream side which indicates that the upstream side is allowed to update input data (to this module) right AFTER the next rising edge of the clock
+    input wire logic i_input_valid, //! valid signal from upstream side
+    input wire logic signed [BIT_WIDTH_IN_A-1:0] i_a, //! Input a.
+    input wire logic signed [BIT_WIDTH_IN_B-1:0] i_b, //! Input b.
+    //! @end
+    //! @virtualbus ds_side_if @dir out downstream side interface
+    //! ready signal from downstream side which indicates that this module is allowed to update input data (to downstream side) right AFTER the next rising edge of the clock
+    input wire logic i_ds_ready,
+    output wire logic o_output_valid, //! output valid signal
+    output wire logic signed [BIT_WIDTH_OUT-1:0] o_c //! output c = ab
+    //! @end
+);
+// ---------- parameters ----------
+localparam int unsigned CYCLE_LATENCY = MULT_INPUT_STG_PIPELINE_DEPTH + MULT_OUTPUT_STG_PIPELINE_DEPTH + ENABLE_ROUNDING_HALF_TO_EVEN; //! cycle latency
+localparam int unsigned BIT_WIDTH_INTERM_PROD = BIT_WIDTH_IN_A + BIT_WIDTH_IN_B; //! bit width of the intermediate product
+// --------------------
+
+// ---------- parameter validation ----------
+generate
+    if (BIT_SLICE_OFFSET_OUT + BIT_WIDTH_OUT - 1 > BIT_WIDTH_INTERM_PROD - 1) begin: gen_bit_slice_range_validation
+        nonexistent_module_to_throw_a_custom_error_message_for invalid_bit_slice_range();
+    end
+
+    if (MULT_INPUT_STG_PIPELINE_DEPTH < 1) begin: gen_mult_input_stg_pipeline_depth_validation
+        nonexistent_module_to_throw_a_custom_error_message_for invalid_mult_input_stg_pipeline_depth();
+    end
+
+    if (MULT_OUTPUT_STG_PIPELINE_DEPTH < 1) begin: gen_mult_output_stg_pipeline_depth_validation
+        nonexistent_module_to_throw_a_custom_error_message_for invalid_mult_output_stg_pipeline_depth();
+    end
+
+    if (ENABLE_ROUNDING_HALF_TO_EVEN && BIT_SLICE_OFFSET_OUT == 0) begin: gen_rounding_param_validation
+        nonexistent_module_to_throw_a_custom_error_message_for invalid_rounding_params();
+    end
+endgenerate
+// --------------------
+
+// ---------- functions ----------
+// --------------------
+
+// ---------- signals and storage ----------
+var logic [CYCLE_LATENCY-1:0] r_vld_dly_line; //! delay line for the output valid signal
+wire g_can_adv_pip_ln; //! signal indicating that the pipeline can advance
+assign g_can_adv_pip_ln = !r_vld_dly_line[CYCLE_LATENCY-1] || i_ds_ready;
+wire g_adv_pip_ln; //! signal indicating that the pipeline should advance
+assign g_adv_pip_ln = i_input_valid & g_can_adv_pip_ln;
+
+var logic [MULT_INPUT_STG_PIPELINE_DEPTH-1:0][BIT_WIDTH_IN_A-1:0] r_a; //! multiplier input stage pipeline register for a
+var logic [MULT_INPUT_STG_PIPELINE_DEPTH-1:0][BIT_WIDTH_IN_B-1:0] r_b; //! multiplier input stage pipeline register for b
+var logic [MULT_OUTPUT_STG_PIPELINE_DEPTH-1:0][BIT_WIDTH_INTERM_PROD-1:0] r_c_pre_slc_rnd; //! multiplier output stage pipeline register for c before slicing/rounding
+generate
+    if (ENABLE_ROUNDING_HALF_TO_EVEN) begin: gen_rnd_sigs
+        wire signed [BIT_WIDTH_INTERM_PROD-BIT_SLICE_OFFSET_OUT-1:0] g_round_res; //! result of rounding
+        var logic signed [BIT_WIDTH_OUT-1:0] r_c_post_rnd_slc; //! c after rounding&slicing
+    end
+endgenerate
+// --------------------
+
+// ---------- instances ----------
+generate
+    if (ENABLE_ROUNDING_HALF_TO_EVEN) begin: gen_round
+        round_hf2evn_v1_0_0 #(
+            .N(BIT_WIDTH_INTERM_PROD),
+            .N_F(BIT_SLICE_OFFSET_OUT)
+        ) round_hf2evn (
+            .i_val(r_c_pre_slc_rnd[MULT_OUTPUT_STG_PIPELINE_DEPTH-1]),
+            .o_val(gen_rnd_sigs.g_round_res)
+        );
+    end
+endgenerate
+// --------------------
+
+// ---------- Drive output signals. ----------
+assign o_ready = g_can_adv_pip_ln;
+assign o_output_valid = i_input_valid & r_vld_dly_line[CYCLE_LATENCY-1];
+generate
+    if (ENABLE_ROUNDING_HALF_TO_EVEN) begin: gen_out_rnd
+        assign o_c = gen_rnd_sigs.r_c_post_rnd_slc;
+    end else begin: gen_out_trunc
+        assign o_c = signed'(r_c_pre_slc_rnd[MULT_OUTPUT_STG_PIPELINE_DEPTH-1][BIT_SLICE_OFFSET_OUT +: BIT_WIDTH_OUT]);
+    end
+endgenerate
+// --------------------
+
+// ---------- blocks ----------
+//! Updates valid delay line.
+always_ff @(posedge i_clk) begin: blk_update_vld_dly_line
+    if (i_sync_rst) begin
+        r_vld_dly_line <= '0;
+    end else if (g_adv_pip_ln) begin
+        r_vld_dly_line <= {r_vld_dly_line[CYCLE_LATENCY-2:0], 1'b1};
+    end
+end
+
+//! Updates multiplier input stage pipeline registers.
+always_ff @(posedge i_clk) begin: blk_update_mult_input_stg_ppln_regs
+    if (i_sync_rst) begin
+        r_a <= '0;
+        r_b <= '0;
+    end else if (g_adv_pip_ln) begin
+        // Advances pipeline.
+        for (int unsigned d=MULT_INPUT_STG_PIPELINE_DEPTH-1; d>0; --d) begin
+            r_a[d] <= r_a[d-1];
+            r_b[d] <= r_b[d-1];
+        end
+
+        r_a[0] <= i_a;
+        r_b[0] <= i_b;
+    end
+end
+
+//! Updates multiplier output stage pipeline registers.
+always_ff @(posedge i_clk) begin: blk_update_mult_output_stg_ppln_regs
+    if (i_sync_rst) begin
+        r_c_pre_slc_rnd <= '0;
+    end else if (g_adv_pip_ln) begin
+        // Advances pipeline.
+        for (int unsigned d=MULT_OUTPUT_STG_PIPELINE_DEPTH-1; d>0; --d) begin
+            r_c_pre_slc_rnd[d] <= r_c_pre_slc_rnd[d-1];
+        end
+
+        r_c_pre_slc_rnd[0] <= BIT_WIDTH_INTERM_PROD'(signed'(r_a[MULT_INPUT_STG_PIPELINE_DEPTH-1])) * BIT_WIDTH_INTERM_PROD'(signed'(r_b[MULT_INPUT_STG_PIPELINE_DEPTH-1]));
+    end
+end
+
+//! Updates post-rounding registers.
+generate
+    if (ENABLE_ROUNDING_HALF_TO_EVEN) begin: gen_update_post_rnd_regs
+        always_ff @(posedge i_clk) begin: blk_update_post_rnd_regs
+            if (i_sync_rst) begin
+                gen_rnd_sigs.r_c_post_rnd_slc <= '0;
+            end if (g_adv_pip_ln) begin
+                gen_rnd_sigs.r_c_post_rnd_slc <= gen_rnd_sigs.g_round_res[BIT_WIDTH_OUT-1:0];
+            end
+        end
+    end
+endgenerate
+// --------------------
+endmodule
+
+`default_nettype wire
---
+++ FPGA/synth/math/multiplier/src/tb/test_bench_fxd_pt_mul_v2_0_0.sv
@@ -0,0 +1,199 @@
+// Verible directive
+// verilog_lint: waive-start parameter-name-style
+// verilog_lint: waive-start line-length
+
+`default_nettype none
+
+// timescale is defined in Makefile.
+
+module test_bench_fxd_pt_mul_v2_0_0;
+// ---------- parameters ----------
+localparam int unsigned CLK_PERIOD_NS = 8; //! clock period in ns
+localparam int unsigned SIM_TIME_LIMIT_NS = 200; //! simulation time limit in ns
+localparam int unsigned RST_DURATION_CYCLE = 1; //! reset duration in cycles
+
+localparam int unsigned DUT_BIT_WIDTH_IN_A = 16; //! Details are in the DUT documentation.
+localparam int unsigned DUT_BIT_WIDTH_IN_B = 16; //! ditto
+localparam int unsigned DUT_BIT_WIDTH_OUT = 12; //! ditto
+localparam int unsigned DUT_BIT_SLICE_OFFSET_OUT = 15; //! ditto
+localparam int unsigned DUT_MULT_INPUT_STG_PIPELINE_DEPTH = 1; //! ditto
+localparam int unsigned DUT_MULT_OUTPUT_STG_PIPELINE_DEPTH = 1; //! ditto
+localparam bit DUT_ENABLE_ROUNDING_HALF_TO_EVEN = 1'b1; //! ditto
+localparam int unsigned DUT_CYCLE_LAT = DUT_MULT_INPUT_STG_PIPELINE_DEPTH + DUT_MULT_OUTPUT_STG_PIPELINE_DEPTH + DUT_ENABLE_ROUNDING_HALF_TO_EVEN; //! DUT cycle latency
+// --------------------
+
+// ---------- parameter validation ----------
+// --------------------
+
+// ---------- functions ----------
+// --------------------
+
+// ---------- signals and storage ----------
+interface dut_if #(
+    parameter int unsigned BIT_WIDTH_IN_A = 16,
+    parameter int unsigned BIT_WIDTH_IN_B = 16,
+    parameter int unsigned BIT_WIDTH_OUT = 16,
+    parameter int unsigned BIT_SLICE_OFFSET_OUT = 0
+)(
+    input wire logic i_clk //! clock signal
+);
+    // interface between upstream-side and DUT
+    logic ready_to_us;
+    logic input_valid;
+    logic signed [BIT_WIDTH_IN_A-1:0] a;
+    logic signed [BIT_WIDTH_IN_B-1:0] b;
+
+    // interface between DUT and downstream-side and DUT
+    logic ready_from_ds;
+    logic output_valid;
+    logic signed [BIT_WIDTH_OUT-1:0] c;
+
+    task automatic reset_bench_driven_sigs();
+        input_valid <= 1'b0;
+        a <= '0;
+        b <= '0;
+        ready_from_ds <= 1'b0;
+    endtask
+endinterface
+
+typedef virtual interface dut_if #(
+    .BIT_WIDTH_IN_A(DUT_BIT_WIDTH_IN_A),
+    .BIT_WIDTH_IN_B(DUT_BIT_WIDTH_IN_B),
+    .BIT_WIDTH_OUT(DUT_BIT_WIDTH_OUT),
+    .BIT_SLICE_OFFSET_OUT(DUT_BIT_SLICE_OFFSET_OUT)
+) dut_vif_t;
+
+var bit r_clk; //! clock signal
+var bit r_sync_rst; //! clock synchronous reset signal
+
+dut_vif_t dut_vif; //! virtual interface to DUT
+// --------------------
+
+// ---------- instances ----------
+//! interface to DUT
+dut_if #(
+    .BIT_WIDTH_IN_A(DUT_BIT_WIDTH_IN_A),
+    .BIT_WIDTH_IN_B(DUT_BIT_WIDTH_IN_B),
+    .BIT_WIDTH_OUT(DUT_BIT_WIDTH_OUT),
+    .BIT_SLICE_OFFSET_OUT(DUT_BIT_SLICE_OFFSET_OUT)
+) dut_if_0 (.i_clk(r_clk));
+
+//! DUT instance
+fxd_pt_mul_v2_0_0 #(
+    .BIT_WIDTH_IN_A(DUT_BIT_WIDTH_IN_A),
+    .BIT_WIDTH_IN_B(DUT_BIT_WIDTH_IN_B),
+    .BIT_WIDTH_OUT(DUT_BIT_WIDTH_OUT),
+    .BIT_SLICE_OFFSET_OUT(DUT_BIT_SLICE_OFFSET_OUT),
+    .MULT_INPUT_STG_PIPELINE_DEPTH(DUT_MULT_INPUT_STG_PIPELINE_DEPTH),
+    .MULT_OUTPUT_STG_PIPELINE_DEPTH(DUT_MULT_OUTPUT_STG_PIPELINE_DEPTH),
+    .ENABLE_ROUNDING_HALF_TO_EVEN(DUT_ENABLE_ROUNDING_HALF_TO_EVEN)
+) dut (
+    .i_clk(r_clk),
+    .i_sync_rst(r_sync_rst),
+
+    .o_ready(dut_if_0.ready_to_us),
+    .i_input_valid(dut_if_0.input_valid),
+    .i_a(dut_if_0.a),
+    .i_b(dut_if_0.b),
+
+    .i_ds_ready(dut_if_0.ready_from_ds),
+    .o_output_valid(dut_if_0.output_valid),
+    .o_c(dut_if_0.c)
+);
+// --------------------
+
+// ---------- blocks ----------
+//! Drives the clock.
+initial forever #(CLK_PERIOD_NS/2) r_clk = ~r_clk;
+
+//! Drives the reset signal.
+task automatic drive_rst(ref dut_vif_t vif);
+    @(posedge r_clk);
+    r_sync_rst <= 1'b1;
+    vif.reset_bench_driven_sigs();
+    repeat (RST_DURATION_CYCLE) begin
+        @(posedge r_clk);
+    end
+    r_sync_rst <= 1'b0;
+endtask
+
+// Feeds data to DUT.
+task automatic feed_data(ref dut_vif_t vif);
+    localparam int NUM_TEST_CASES = 16;
+
+    typedef struct packed {
+        logic signed [DUT_BIT_WIDTH_IN_A-1:0] a;
+        logic signed [DUT_BIT_WIDTH_IN_B-1:0] b;
+        logic signed [DUT_BIT_WIDTH_OUT-1:0] c_expected;
+    } test_case_t;
+
+    test_case_t [NUM_TEST_CASES-1:0] testCases = {
+        {DUT_BIT_WIDTH_IN_A'(588),     DUT_BIT_WIDTH_IN_B'(17_642),  DUT_BIT_WIDTH_OUT'(317)},
+        {DUT_BIT_WIDTH_IN_A'(-12_752), DUT_BIT_WIDTH_IN_B'(-14_846), DUT_BIT_WIDTH_OUT'(1_681)},
+        {DUT_BIT_WIDTH_IN_A'(-3_929),  DUT_BIT_WIDTH_IN_B'(8_848),   DUT_BIT_WIDTH_OUT'(-1_061)},
+        {DUT_BIT_WIDTH_IN_A'(26_299),  DUT_BIT_WIDTH_IN_B'(3_232),   DUT_BIT_WIDTH_OUT'(-1_502)},
+        {DUT_BIT_WIDTH_IN_A'(16_181),  DUT_BIT_WIDTH_IN_B'(-2_016),  DUT_BIT_WIDTH_OUT'(-996)},
+        {DUT_BIT_WIDTH_IN_A'(-1_592),  DUT_BIT_WIDTH_IN_B'(-19_853), DUT_BIT_WIDTH_OUT'(965)},
+        {DUT_BIT_WIDTH_IN_A'(17_979),  DUT_BIT_WIDTH_IN_B'(-13_185), DUT_BIT_WIDTH_OUT'(958)},
+        {DUT_BIT_WIDTH_IN_A'(32_732),  DUT_BIT_WIDTH_IN_B'(8_110),   DUT_BIT_WIDTH_OUT'(-91)},
+        {DUT_BIT_WIDTH_IN_A'(9_709),   DUT_BIT_WIDTH_IN_B'(25_513),  DUT_BIT_WIDTH_OUT'(-633)},
+        {DUT_BIT_WIDTH_IN_A'(-9_118),  DUT_BIT_WIDTH_IN_B'(15_883),  DUT_BIT_WIDTH_OUT'(-324)},
+        {DUT_BIT_WIDTH_IN_A'(6_277),   DUT_BIT_WIDTH_IN_B'(-19_474), DUT_BIT_WIDTH_OUT'(366)},
+        {DUT_BIT_WIDTH_IN_A'(11_137),  DUT_BIT_WIDTH_IN_B'(26_767),  DUT_BIT_WIDTH_OUT'(905)},
+        {DUT_BIT_WIDTH_IN_A'(-27_617), DUT_BIT_WIDTH_IN_B'(-31_356), DUT_BIT_WIDTH_OUT'(1_851)},
+        {DUT_BIT_WIDTH_IN_A'(29_375),  DUT_BIT_WIDTH_IN_B'(13_770),  DUT_BIT_WIDTH_OUT'(56)},
+        {DUT_BIT_WIDTH_IN_A'(32_200),  DUT_BIT_WIDTH_IN_B'(17_126),  DUT_BIT_WIDTH_OUT'(445)},
+        {DUT_BIT_WIDTH_IN_A'(5_910),   DUT_BIT_WIDTH_IN_B'(-1_591),  DUT_BIT_WIDTH_OUT'(-287)}
+    };
+
+    bit is_error = 1'b0;
+    int unsigned cnt_input = 0;
+    int unsigned cnt_output = 0;
+
+    while (cnt_output < NUM_TEST_CASES) begin
+        if (cnt_input < NUM_TEST_CASES) begin
+            vif.input_valid <= 1'b1;
+            vif.a <= testCases[cnt_input].a;
+            vif.b <= testCases[cnt_input].b;
+            vif.ready_from_ds <= 1'b1;
+            cnt_input += 1;
+        end
+        if (cnt_output < NUM_TEST_CASES && vif.output_valid) begin
+            if (cnt_output == 0) begin
+                assert(cnt_input == DUT_CYCLE_LAT + 2) else $fatal(2, "cnt_input = %0d, expected = %0d", cnt_input, DUT_CYCLE_LAT + 1);
+            end
+            if (vif.c != testCases[cnt_output].c_expected) begin
+                $error("Test case %0d failed: c = %0d, expected = %0d", cnt_output, vif.c, testCases[cnt_output].c_expected);
+                is_error = 1'b1;
+            end
+            cnt_output += 1;
+        end
+        @(posedge r_clk);
+    end
+
+    if (!is_error) begin
+        $display("All test cases passed.");
+    end
+endtask
+
+task automatic scenario();
+    drive_rst(dut_vif);
+    @(posedge r_clk);
+    feed_data(dut_vif);
+    @(posedge r_clk);
+    $finish;
+endtask
+
+//! Launches scenario and manage time limit.
+initial begin
+    dut_vif = dut_if_0;
+    fork
+        scenario();
+    join_none
+    #SIM_TIME_LIMIT_NS;
+    $fatal(2, "Simulation timeout.");
+end
+// --------------------
+endmodule
+
+`default_nettype wire
