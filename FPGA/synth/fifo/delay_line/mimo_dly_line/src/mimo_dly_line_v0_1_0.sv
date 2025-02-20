f1fdaac70678c0b9dea594c9b276fe89c264e3b8
motchy <motchy869@gmail.com>
2025-02-20 21:40:56 +0900

    feat: mimo_dly_line_v0_1_0

---
+++ FPGA/synth/fifo/delay_line/mimo_dly_line/src/mimo_dly_line_v0_1_0.sv
@@ -0,0 +1,155 @@
+// Verible directive
+// verilog_lint: waive-start parameter-name-style
+// verilog_lint: waive-start line-length
+
+`ifndef MIMO_DLY_LINE_V0_1_0_SV_INCLUDED
+`define MIMO_DLY_LINE_V0_1_0_SV_INCLUDED
+
+`default_nettype none
+
+//! A MIMO (multiple-input multiple-output) delay line.
+//! All outputs are **buffered**.
+//!
+//! This module receives elements from the upstream side up to ```MAX_N_C``` elements at one clock cycle, and discharges up to ```MAX_N_DC``` elements at one clock cycle.
+//! There is no handshake feature, so a parent module must handle the flow control.
+module mimo_dly_line_v0_1_0 #(
+    parameter bit BE_UNSAFE = 1'b0, //! Enable unsafe configuration, which disables most of costly invalid-value sanitizing, improving place&route feasibility.
+    parameter int unsigned L = 16, //! delay line length (>0)
+    parameter int unsigned MAX_N_C = 4, //! maximal instantaneous number of charging elements, must be in range [1,```L```]
+    parameter int unsigned MAX_N_DC = 4, //! maximal instantaneous number of discharging elements, must be in range [1,```L```]
+    `ifndef COMPILER_MATURITY_LEVEL_0 // This macro should be set MANUALLY in the project settings if needed.
+        parameter type T_E = logic [7:0] //! element data type
+    `else
+        parameter int unsigned BW_ELEM = 8 //! element data bit width
+    `endif
+)(
+    //! @virtualbus cont_if @dir in control interface
+    input wire logic i_clk, //! input clock
+    input wire logic i_sync_rst, //! input reset signal synchronous to the input clock
+    input wire logic i_freeze, //! Freeze directive, which stops all state transitions except for the reset. this signal can be used to flow control by the parent module.
+    input wire logic [$clog2(L+1)-1:0] i_n_init_0_elems, //! number of the initial '0 data elements in delay line, **clipped** to the range [0,```L```]
+    output wire logic [$clog2(L+1)-1:0] o_cnt, //! current number of elements in the delay line
+    output wire logic [$clog2(L+1)-1:0] o_cnt_free, //! number of free slots in the delay line
+    input wire logic [$clog2(MAX_N_C+1)-1:0] i_n_c, //! Number of charging elements. At the next clock rising-edge, ```i_n_c``` elements are added to the delay line. **MUST NOT** be greater than ```o_cnt_free```. Under safe configuration, ```i_n_c``` is **clipped** to the range [0,```o_cnt_free```]. Under unsafe configuration, invalid value will cause **UNDEFINED** behavior.
+    input wire logic [$clog2(MAX_N_DC+1)-1:0] i_n_dc, //! Number of discharging elements, **MUST NOT** be greater than ```o_cnt```. At the next clock rising-edge, ```i_n_dc``` elements are removed from the delay line. Under safe configuration, ```i_n_dc``` is **clipped** to the range [0,```o_cnt```]. Under unsafe configuration, invalid value will cause **UNDEFINED** behavior.
+    //! @end
+    //! @virtualbus data_in_if @dir in data input interface
+    `ifndef COMPILER_MATURITY_LEVEL_0
+        //! Charging elements. First ```i_n_c``` elements are assumed to be valid.
+        input wire T_E [MAX_N_C-1:0] i_c_elems,
+    `else
+        //! Charging elements. First ```i_n_c``` elements are assumed to be valid.
+        input T_E [MAX_N_C-1:0] i_c_elems,
+    `endif
+    //! @end
+    //! @virtualbus data_out_if @dir out data output interface
+    `ifndef COMPILER_MATURITY_LEVEL_0
+        //! Delay line. Only first ```o_cnt``` elements are valid.
+        output wire T_E [L-1:0] o_line
+    `else
+        //! Delay line. Only first ```o_cnt``` elements are valid.
+        output T_E [L-1:0] o_line
+    `endif
+    //! @end
+);
+// ---------- imports ----------
+// --------------------
+
+// ---------- parameters ----------
+// --------------------
+
+// ---------- parameter validation ----------
+generate
+    if (L < 1) begin: gen_L_lower_bound_validation
+        nonexistent_module_to_throw_a_custom_error_message_for too_small_L();
+    end
+    if (MAX_N_C > L) begin: gen_MAX_N_C_upper_bound_validation
+        nonexistent_module_to_throw_a_custom_error_message_for too_large_MAX_N_C();
+    end
+    if (MAX_N_DC > L) begin: gen_MAX_N_DC_upper_bound_validation
+        nonexistent_module_to_throw_a_custom_error_message_for too_large_MAX_N_DC();
+    end
+endgenerate
+// --------------------
+
+// ---------- brief sketch ----------
+// --------------------
+
+// ---------- functions ----------
+// --------------------
+
+// ---------- signals and storage ----------
+`ifdef COMPILER_MATURITY_LEVEL_0
+    typedef logic [BW_ELEM-1:0] T_E; //! element data type
+`endif
+
+var T_E [L-1:0] r_line; //! line buffer, element-slots
+var logic [$clog2(L+1)-1:0] r_cnt; //! number of elements in the delay line
+var logic [$clog2(L+1)-1:0] r_cnt_free; //! number of free slots in the delay line
+
+wire [$clog2(MAX_N_C+1)-1:0] w_snt_n_c; //! Sanitized version of ```i_n_c```. Under safe configuration, it is **clipped** to the range [0,```o_cnt_free```]. Under unsafe configuration, this is merely a pass-through.
+wire [$clog2(MAX_N_DC+1)-1:0] w_snt_n_dc; //! Sanitized version of ```i_n_dc```. Under safe configuration, it is **clipped** to the range [0,```o_cnt```]. Under unsafe configuration, this is merely a pass-through.
+
+generate
+    if (BE_UNSAFE) begin: gen_unsafe
+        assign w_snt_n_c = i_n_c;
+        assign w_snt_n_dc = i_n_dc;
+    end else begin: gen_safe
+        assign w_snt_n_c = ($clog2(L+1)'(i_n_c) > r_cnt_free) ? $clog2(MAX_N_C+1)'(r_cnt_free) : i_n_c;
+        assign w_snt_n_dc = ($clog2(L+1)'(i_n_dc) > r_cnt) ? $clog2(MAX_N_DC+1)'(r_cnt) : i_n_dc;
+    end
+endgenerate
+// --------------------
+
+// ---------- instances ----------
+// --------------------
+
+// ---------- Drives output signals. ----------
+assign o_cnt = r_cnt;
+assign o_cnt_free = r_cnt_free;
+assign o_line = r_line;
+// --------------------
+
+// ---------- blocks ----------
+//! Updates the number of elements and free slots in the delay line.
+always_ff @(posedge i_clk) begin: blk_update_cnt_and_free
+    if (i_sync_rst) begin
+        automatic logic [$clog2(L+1)-1:0] cnt_rst = (i_n_init_0_elems > $clog2(L+1)'(L)) ? $clog2(L+1)'(L) : i_n_init_0_elems;
+        r_cnt <= cnt_rst;
+        r_cnt_free <= $clog2(L+1)'(L) - cnt_rst;
+    end else if (!i_freeze) begin
+        // if (!BE_UNSAFE) begin // just for debug
+        //     if (w_snt_n_c != i_n_c) begin
+        //         $fatal(2, "[time=%0t] value mismatch, w_snt_n_c: %0d, i_n_c: %0d", $realtime, w_snt_n_c, i_n_c);
+        //     end
+        //     if (w_snt_n_dc != i_n_dc) begin
+        //         $fatal(2, "[time=%0t] value mismatch, w_snt_n_dc: %0d, i_n_dc: %0d", $realtime, w_snt_n_dc, i_n_dc);
+        //     end
+        // end
+        r_cnt <= r_cnt + $clog2(L+1)'(w_snt_n_c) - $clog2(L+1)'(w_snt_n_dc);
+        r_cnt_free <= r_cnt_free + $clog2(L+1)'(w_snt_n_dc) - $clog2(L+1)'(w_snt_n_c);
+    end
+end
+
+//! Updates the delay line.
+always_ff @(posedge i_clk) begin: blk_update_line
+    if (i_sync_rst) begin
+        r_line <= '{default:'0};
+    end else if (!i_freeze) begin
+        localparam int unsigned BW1 = $clog2(L + MAX_N_DC);
+        for (int unsigned i=0; i<L; ++i) begin
+            automatic logic [BW1-1:0] src_idx_a = BW1'(i) + BW1'(w_snt_n_dc);
+            automatic logic [BW1-1:0] src_idx_b = src_idx_a - BW1'(r_cnt);
+            r_line[i] <= (src_idx_a < BW1'(r_cnt)) ? r_line[src_idx_a] : ( // Old element is used. This line causes `Warning (10027)` in Quartus Prime Lite 23.1std, but it is safe.
+                (src_idx_b < BW1'(w_snt_n_c)) ? i_c_elems[src_idx_b] : // Input element is used.
+                '{default:'0} // Nothing can be used. Clears the slot.
+            );
+        end
+    end
+end
+// --------------------
+endmodule
+
+`default_nettype wire
+
+`endif // MIMO_DLY_LINE_V0_1_0_SV_INCLUDED
---
+++ FPGA/synth/fifo/delay_line/mimo_dly_line/src/tb/test_bench.sv
@@ -0,0 +1,248 @@
+// Verible directive
+// verilog_lint: waive-start parameter-name-style
+// verilog_lint: waive-start line-length
+
+`include "../mimo_dly_line_v0_1_0.sv"
+
+`default_nettype none
+
+// timescale is defined in Makefile.
+
+//! interface to DUT
+interface dut_if #(
+    parameter bit BE_UNSAFE = 1'b0,
+    parameter int unsigned L = 16,
+    parameter int unsigned MAX_N_C = 4,
+    parameter int unsigned MAX_N_DC = 4,
+    parameter type T_E = logic [7:0]
+)(
+    input wire logic i_clk //! clock signal
+);
+    // signals between bench and DUT's control interface
+    logic freeze;
+    logic [$clog2(L+1)-1:0] n_init_0_elems;
+    logic [$clog2(L+1)-1:0] cnt;
+    logic [$clog2(L+1)-1:0] cnt_free;
+    logic [$clog2(MAX_N_C+1)-1:0] n_c;
+    logic [$clog2(MAX_N_DC+1)-1:0] n_dc;
+
+    // signals between bench and DUT's data input interface
+    T_E [MAX_N_C-1:0] c_elems;
+
+    // signals between bench and DUT's data output interface
+    T_E [L-1:0] line;
+
+    task automatic reset_bench_driven_sigs(
+        input logic [$clog2(L+1)-1:0] _n_init_0_elems = '0
+    );
+        // verilator lint_off INITIALDLY
+        freeze <= 1'b0;
+        n_init_0_elems <= _n_init_0_elems;
+        n_c <= '0;
+        n_dc <= '0;
+        c_elems <= '0;
+        // verilator lint_on INITIALDLY
+    endtask
+endinterface
+
+//! test bench
+module test_bench;
+// ---------- parameters ----------
+localparam int unsigned CLK_PERIOD_NS = 8; //! clock period in ns
+localparam int unsigned DELTA_T = 1; //! delta time in ns, mainly used for sampling DUT's outputs
+localparam int unsigned RST_DURATION_CYCLE = 1; //! reset duration in clock cycle
+
+//localparam bit BE_UNSAFE = 1'b1; //! Enable unsafe configuration.
+localparam bit BE_UNSAFE = 1'b0; //! Disable unsafe configuration.
+localparam int unsigned L = 11; //! delay line length
+localparam int unsigned MAX_N_C = 7; //! maximal instantaneous number of charging elements
+localparam int unsigned MIN_N_C = 1; //! minimal instantaneous number of charging elements
+localparam int unsigned MAX_N_DC = 5; //! maximal instantaneous number of discharging elements
+localparam int unsigned MIN_N_DC = 1; //! minimal instantaneous number of discharging elements
+localparam int unsigned N_TEST_ELEMS = 50; //! number of test elements
+
+localparam int unsigned N_TEMP1 = MIN_N_C < MIN_N_DC ? MIN_N_C : MIN_N_DC; //! temporary variable
+localparam int unsigned SIM_TIME_LIMIT_NS = (RST_DURATION_CYCLE + (N_TEST_ELEMS + N_TEMP1 - 1)/N_TEMP1)*CLK_PERIOD_NS*11/10; //! simulation time limit in ns
+
+typedef logic [7:0] T_E; //! element data type
+// --------------------
+
+// ---------- parameter validation ----------
+// --------------------
+
+// ---------- functions ----------
+// --------------------
+
+// ---------- signals and storage ----------
+var bit r_clk; //! clock signal
+var bit r_sync_rst; //! clock synchronous reset signal
+
+//! virtual interface to DUT
+typedef virtual interface dut_if #(
+    .BE_UNSAFE(BE_UNSAFE),
+    .L(L),
+    .MAX_N_C(MAX_N_C),
+    .MAX_N_DC(MAX_N_DC),
+    .T_E(T_E)
+) dut_vif_t;
+
+dut_vif_t dut_vif; //! virtual interface to DUT
+// --------------------
+
+// ---------- instances ----------
+//! interface to DUT
+dut_if #(
+    .BE_UNSAFE(BE_UNSAFE),
+    .L(L),
+    .MAX_N_C(MAX_N_C),
+    .MAX_N_DC(MAX_N_DC),
+    .T_E(T_E)
+) dut_if (.i_clk(r_clk));
+
+mimo_dly_line_v0_1_0 #(
+    .BE_UNSAFE(BE_UNSAFE),
+    .L(L),
+    .MAX_N_C(MAX_N_C),
+    .MAX_N_DC(MAX_N_DC),
+    .T_E(T_E)
+) dut (
+    .i_clk(r_clk),
+    .i_sync_rst(r_sync_rst),
+    .i_freeze(dut_if.freeze),
+    .i_n_init_0_elems(dut_if.n_init_0_elems),
+    .o_cnt(dut_if.cnt),
+    .o_cnt_free(dut_if.cnt_free),
+    .i_n_c(dut_if.n_c),
+    .i_n_dc(dut_if.n_dc),
+    .i_c_elems(dut_if.c_elems),
+    .o_line(dut_if.line)
+);
+// --------------------
+
+// ---------- blocks ----------
+//! Drives the clock.
+initial forever #(CLK_PERIOD_NS/2) r_clk = ~r_clk;
+
+//! Drives the reset signal.
+task automatic drive_rst(ref dut_vif_t vif);
+    // verilator lint_off INITIALDLY
+    @(posedge r_clk);
+    r_sync_rst <= 1'b1;
+    vif.reset_bench_driven_sigs();
+    repeat (RST_DURATION_CYCLE) begin
+        @(posedge r_clk);
+    end
+    r_sync_rst <= 1'b0;
+    // verilator lint_on INITIALDLY
+endtask
+
+// Feeds data to DUT.
+task automatic feed_data(ref dut_vif_t vif);
+    localparam int unsigned BW_ELEM = $bits(T_E);
+    bit is_error = 1'b0;
+    int cnt_charged_elems = 0;
+    int cnt_discharged_elems = 0;
+    T_E test_elems[] = new[N_TEST_ELEMS];
+    T_E test_discharged_elems[] = new[N_TEST_ELEMS];
+
+    for (int i=0; i<N_TEST_ELEMS; ++i) begin
+        test_elems[i] = BW_ELEM'($urandom_range(2**BW_ELEM-1, 0));
+    end
+
+    while (1'b1) begin
+        @(posedge r_clk) begin
+            // Assumes that the previous request is processed.
+            cnt_charged_elems += int'(vif.n_c);
+
+            // Collects the discharging elements.
+            if (cnt_discharged_elems < N_TEST_ELEMS) begin
+                int num_discharging_elems = int'(vif.n_dc);
+                // $write("Collecting: ");
+                for (int unsigned i=0; i<num_discharging_elems; ++i) begin
+                    test_discharged_elems[cnt_discharged_elems+i] = vif.line[i];
+                    // $write("%02h, ", vif.line[i]);
+                end
+                cnt_discharged_elems += num_discharging_elems;
+                // $display("  Collected %0d elements. cnt_discharged_elems: %0d", num_discharging_elems, cnt_discharged_elems);
+                assert (cnt_discharged_elems <= N_TEST_ELEMS) else $fatal(2, "Too many elements are popped.");
+            end
+        end
+
+        #DELTA_T;
+
+        // Shows the next charging request.
+        if (cnt_charged_elems < N_TEST_ELEMS) begin
+            // Determines the number of elements to charge.
+            int num_charging_elems = $urandom_range(MAX_N_C, MIN_N_C);
+            if (N_TEST_ELEMS - cnt_charged_elems < num_charging_elems) begin // last chunk
+                num_charging_elems = N_TEST_ELEMS - cnt_charged_elems;
+            end
+            if (vif.cnt_free < $clog2(L+1)'(num_charging_elems)) begin
+                num_charging_elems = int'(vif.cnt_free);
+            end
+
+            // $write("Charging: ");
+            vif.n_c = $clog2(MAX_N_C+1)'(num_charging_elems);
+            for (int unsigned i=0; i<MAX_N_C; ++i) begin
+                vif.c_elems[i] = (i<num_charging_elems) ? test_elems[cnt_charged_elems+i] : '0;
+                // if (i<num_charging_elems) begin $write("%02h, ", vif.c_elems[i]); end
+            end
+            // $display("  Charging %0d elements. cnt_charged_elems: %0d", num_charging_elems, cnt_charged_elems);
+        end else begin
+            vif.n_c = '0;
+            vif.c_elems = '0;
+        end
+
+        // Shows the next discharging request.
+        if (cnt_discharged_elems < N_TEST_ELEMS) begin
+            // Determines the number of elements to discharge.
+            int num_discharging_elems = $urandom_range(MAX_N_DC, MIN_N_DC);
+            if (N_TEST_ELEMS - cnt_discharged_elems < num_discharging_elems) begin // last chunk
+                num_discharging_elems = N_TEST_ELEMS - cnt_discharged_elems;
+            end
+            if (vif.cnt < $clog2(L+1)'(num_discharging_elems)) begin
+                num_discharging_elems = int'(vif.cnt);
+            end
+
+            vif.n_dc = $clog2(MAX_N_DC+1)'(num_discharging_elems);
+        end else begin
+            vif.n_dc = '0;
+            break;
+        end
+    end
+
+    // Verifies the popped elements.
+    for (int i=0; i<N_TEST_ELEMS; ++i) begin
+        if (test_elems[i] != test_discharged_elems[i]) begin
+            is_error = 1'b1;
+            $error("Mismatched element at index %0d: expected %02h, but got %02h.", i, test_elems[i], test_discharged_elems[i]);
+        end
+    end
+
+    if (!is_error) begin
+        $display("Test passed.");
+    end
+endtask
+
+task automatic scenario();
+    drive_rst(dut_vif);
+    @(posedge r_clk);
+    feed_data(dut_vif);
+    @(posedge r_clk);
+    $finish;
+endtask
+
+//! Launches scenario and manage time limit.
+initial begin
+    $timeformat(-9, 3, " ns", 12);
+    dut_vif = dut_if;
+    fork
+        scenario();
+    join_none
+    #SIM_TIME_LIMIT_NS;
+    $fatal(2, "Simulation timeout.");
+end
+// --------------------
+endmodule
+
+`default_nettype wire
