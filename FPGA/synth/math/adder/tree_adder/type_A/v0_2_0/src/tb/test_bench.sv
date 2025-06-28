d9410a580dc3a45dfc65c236006f5a87842fe764
motchy <motchy869@gmail.com>
2025-06-28 18:56:03 +0900

    feat: tree_adder_typ_a_v0_2_0

---
+++ FPGA/synth/math/adder/tree_adder/type_A/v0_2_0/src/tb/test_bench.sv
@@ -0,0 +1,196 @@
+// Verible directive
+// verilog_lint: waive-start parameter-name-style
+// verilog_lint: waive-start line-length
+
+`include "../tree_adder_typ_a_v0_2_0.sv"
+
+`default_nettype none
+
+// timescale is defined in Makefile.
+
+// verilator lint_off MULTITOP
+
+//! interface to DUT
+interface dut_if #(
+    parameter int unsigned N_IN_ELEMS,
+    parameter int unsigned BW_IN_ELEM
+)(
+    input wire logic i_clk //! clock signal
+);
+    // signals between bench and DUT's control interface
+    logic clk_en;
+
+    // signals between bench and DUT's upstream-side interface
+    logic [N_IN_ELEMS-1:0][BW_IN_ELEM-1:0] elems;
+
+    // signals between bench and DUT's downstream-side interface
+    logic out_vld;
+    logic signed [(BW_IN_ELEM-1)+$clog2(N_IN_ELEMS+1)+1-1:0] sum;
+
+    task automatic reset_bench_driven_sigs();
+        // verilator lint_off INITIALDLY
+        clk_en <= 1'b0;
+        elems <= '{default:'0};
+        // verilator lint_on INITIALDLY
+    endtask
+endinterface
+
+//! test bench
+module test_bench;
+// ---------- parameters ----------
+localparam int unsigned CLK_PRD_NS = 8; //! clock period in ns
+localparam int unsigned DELTA_T = 1; //! delta time in ns, mainly used for sampling DUT's outputs
+localparam int unsigned RST_DUR_CYC = 1; //! reset duration in clock cycle
+localparam int unsigned FRZ_DUR_CYC_AFTER_RST = 1; //! freeze duration in clock cycle after reset
+
+localparam int unsigned N_IN_ELEMS = 9;
+localparam int unsigned BW_IN_ELEM = 8;
+localparam int unsigned BW_SUM = (BW_IN_ELEM-1)+$clog2(N_IN_ELEMS+1)+1; //! bit width of output sum
+localparam int unsigned CYCLE_LATENCY = $clog2(N_IN_ELEMS); //! latency in cycles
+
+localparam int unsigned N_TEST_VECS = 100; //! number of test vectors
+localparam int unsigned SIM_TIME_LIMIT_NS = (RST_DUR_CYC + FRZ_DUR_CYC_AFTER_RST + N_TEST_VECS + CYCLE_LATENCY)*CLK_PRD_NS*11/10; //! simulation time limit in ns
+// --------------------
+
+// ---------- parameter validation ----------
+// --------------------
+
+// ---------- functions ----------
+// --------------------
+
+// ---------- signals and storage ----------
+var bit r_clk; //! clock signal
+var bit r_sync_rst; //! clock synchronous reset signal
+
+//! virtual interface to DUT
+typedef virtual interface dut_if #(
+    .N_IN_ELEMS(N_IN_ELEMS),
+    .BW_IN_ELEM(BW_IN_ELEM)
+) dut_vif_t;
+
+dut_vif_t dut_vif; //! virtual interface to DUT
+
+typedef logic signed [BW_IN_ELEM-1:0] elem_t; //! element data type
+typedef logic signed [BW_SUM-1:0] sum_t; //! sum data type
+// --------------------
+
+// ---------- instances ----------
+//! interface to DUT
+dut_if #(
+    .N_IN_ELEMS(N_IN_ELEMS),
+    .BW_IN_ELEM(BW_IN_ELEM)
+) dut_if (.i_clk(r_clk));
+
+//! DUT instance
+tree_adder_typ_a_v0_2_0 #(
+    .N_IN_ELEMS(N_IN_ELEMS),
+    .BW_IN_ELEM(BW_IN_ELEM),
+    .USE_DSP_RST(1'b0),
+    .USE_CLK_EN(1'b0)
+) dut (
+    .i_clk(r_clk),
+    .i_sync_rst(r_sync_rst),
+    .i_clk_en(dut_if.clk_en),
+    .i_elems(dut_if.elems),
+    .o_out_vld(dut_if.out_vld),
+    .o_sum(dut_if.sum)
+);
+// --------------------
+
+// ---------- blocks ----------
+//! Drives the clock.
+initial forever #(CLK_PRD_NS/2) r_clk = ~r_clk;
+
+//! Drives the reset signal.
+task automatic drive_rst(ref dut_vif_t vif);
+    // verilator lint_off INITIALDLY
+    @(posedge r_clk);
+    r_sync_rst <= 1'b1;
+    vif.reset_bench_driven_sigs();
+    repeat (RST_DUR_CYC) begin
+        @(posedge r_clk);
+    end
+    r_sync_rst <= 1'b0;
+    // verilator lint_on INITIALDLY
+endtask
+
+// Feeds data to DUT.
+task automatic feed_data(ref dut_vif_t vif);
+    typedef struct packed {
+        sum_t actual_sum;
+        sum_t exp_sum;
+        elem_t [N_IN_ELEMS-1:0] elems;
+    } test_vector_t;
+    bit is_error = 1'b0;
+    int unsigned cnt_pushed_vecs = 0;
+    int unsigned cnt_popped_vecs = 0;
+    test_vector_t test_vectors[] = new[N_TEST_VECS];
+
+    // Generates test vectors.
+    for (int i=0; i<N_TEST_VECS; ++i) begin
+        test_vectors[i].exp_sum = 0;
+        for (int j=0; j<N_IN_ELEMS; ++j) begin
+            test_vectors[i].elems[j] = elem_t'($urandom_range(0, 2**BW_IN_ELEM-1) - 2**(BW_IN_ELEM-1));
+            test_vectors[i].exp_sum += sum_t'(test_vectors[i].elems[j]);
+        end
+    end
+
+    // Drives the DUT.
+    while (1'b1) begin
+        #DELTA_T;
+        // vif.clk_en = 1'b1; // Clock-enable is not used in current configuration.
+
+        if (cnt_pushed_vecs < N_TEST_VECS) begin
+            vif.elems = test_vectors[cnt_pushed_vecs].elems;
+            ++cnt_pushed_vecs;
+        end else begin
+            vif.elems = '{default: '0};
+        end
+
+        @(posedge r_clk) begin
+            if (cnt_popped_vecs < N_TEST_VECS) begin
+                if (vif.out_vld) begin
+                    test_vectors[cnt_popped_vecs].actual_sum = vif.sum;
+                    ++cnt_popped_vecs;
+                end
+            end else if (cnt_pushed_vecs == N_TEST_VECS) begin
+                break;
+            end
+        end
+    end
+
+    // Verifies the popped elements.
+    for (int i=0; i<N_TEST_VECS; ++i) begin
+        if (test_vectors[i].actual_sum !== test_vectors[i].exp_sum) begin
+            $display("Error: test vector %0d: expected %0d, got %0d", i, test_vectors[i].exp_sum, test_vectors[i].actual_sum);
+            is_error = 1'b1;
+        end
+    end
+
+    if (!is_error) begin
+        $display("Test passed.");
+    end
+endtask
+
+task automatic scenario();
+    drive_rst(dut_vif);
+    feed_data(dut_vif);
+    $finish;
+endtask
+
+//! Launches scenario and manage time limit.
+initial begin
+    $timeformat(-9, 3, " ns", 12);
+    dut_vif = dut_if;
+    fork
+        scenario();
+    join_none
+    #SIM_TIME_LIMIT_NS;
+    $fatal(2, "Simulation timeout.");
+end
+// --------------------
+endmodule
+
+// verilator lint_on MULTITOP
+
+`default_nettype wire
---
+++ FPGA/synth/math/adder/tree_adder/type_A/v0_2_0/src/tree_adder_typ_a_v0_2_0.sv
@@ -0,0 +1,205 @@
+// Verible directive
+// verilog_lint: waive-start parameter-name-style
+// verilog_lint: waive-start line-length
+
+`ifndef TREE_ADDER_TYP_A_V0_2_0_SV_DEFINED
+`define TREE_ADDER_TYP_A_V0_2_0_SV_DEFINED
+
+`default_nettype none
+
+//! An integer tree-like adder.
+//! This module calculates the sum of a given set of input elements using tree-like adder structure.
+//!
+//! The number of adder is ```N_IN_ELEMS-1``` and the cycle latency is ```$clog2(N_IN_ELEMS)``` where ```N_IN_ELEMS``` is the number of input elements.
+//! The output bit width is ```(BW_IN_ELEM-1) + $clog2(N_IN_ELEMS+1) + 1``` (because the possible minimum sum is ```-N_IN_ELEMS*(2**(BW_IN_ELEM-1))```).
+//! The latency, or tree height, is ```$clog2(N_IN_ELEMS)```.
+//!
+//! There is **no handshake** function, so a parent module must handle the flow control.
+module tree_adder_typ_a_v0_2_0 #(
+    parameter int unsigned N_IN_ELEMS = 9, //! number of input elements, must be 2 or greater
+    parameter int unsigned BW_IN_ELEM = 8, //! bit width of each input element
+    parameter bit USE_DSP_RST = 1'b1, //! Whether to clear the DSP-related numerical data at reset. If not, numerical data registers are not cleared and P&R feasibility will be improved.
+    parameter bit USE_CLK_EN = 1'b1 //! Whether to use clock enable signal. If not, the port exists but not used, and P&R feasibility will be improved.
+)(
+    //! @virtualbus cont_if @dir in control interface
+    //! clock signal
+    input wire logic i_clk,
+    input wire logic i_sync_rst, //! reset signal synchronous to clock
+    input wire logic i_clk_en, //! clock-enable signal
+    //! @end
+    //! @virtualbus us_side_if @dir in upstream side interface
+    input wire logic [N_IN_ELEMS-1:0][BW_IN_ELEM-1:0] i_elems, //! input elements, treated as **SIGNED** internally
+    //! @end
+    //! @virtualbus ds_side_if @dir out downstream side interface
+    //! Signal indicating that the output vector is valid. This is **ONLY** for initial garbage data skipping, **NOT** for flow control.
+    output wire logic o_out_vld,
+    output wire logic signed [(BW_IN_ELEM-1)+$clog2(N_IN_ELEMS+1)+1-1:0] o_sum //! output sum
+    //! @end
+);
+// ---------- imports ----------
+// --------------------
+
+// ---------- parameters ----------
+localparam int unsigned TREE_HEIGHT = $clog2(N_IN_ELEMS); //! tree height
+localparam int unsigned TREE_WIDTH = N_IN_ELEMS/2 + (N_IN_ELEMS%2); //! tree width
+localparam int unsigned BW_OUT = (BW_IN_ELEM-1) + $clog2(N_IN_ELEMS+1) + 1; //! output bit width
+// --------------------
+
+// ---------- parameter validation ----------
+generate
+    if (N_IN_ELEMS < 2) begin: gen_too_small_N_IN_ELEMS
+        nonexistent_module_to_throw_an_error_message_for_too_small_N_IN_ELEMS inst();
+    end
+    if (BW_IN_ELEM < 1) begin: gen_too_small_BW_IN_ELEM
+        nonexistent_module_to_throw_an_error_message_for_too_small_BW_IN_ELEM inst();
+    end
+endgenerate
+// --------------------
+
+// ---------- brief sketch ----------
+// --------------------
+
+// ---------- functions ----------
+typedef logic signed [BW_OUT-1:0] min_max_t;
+typedef logic [31:0] uint_t;
+
+typedef struct packed {
+    uint_t [TREE_HEIGHT-1:0] n_elems; // number of elements in each depth
+    logic [TREE_HEIGHT-1:0][TREE_WIDTH-1:0] type_mat; // Type matrix, the (i,j)-th element stores the type of the element at that position. 1 for adder, 0 for pass-through
+    min_max_t [TREE_HEIGHT-1:0][TREE_WIDTH-1:0] min_val_mat; // minimum value matrix, the (i,j)-th element stores the minimum value of the adder/pass-through at that position
+    uint_t [TREE_HEIGHT-1:0][TREE_WIDTH-1:0] bw_mat; // bit width matrix
+} adder_plan_t;
+
+//! Counts the number of leading ones in a given value.
+function automatic int unsigned cntLdgOnes(input logic [BW_OUT-1:0] val);
+    int unsigned cnt = 0;
+    for (int i=BW_OUT-1; i>=0; --i) begin
+        if (val[i] == 1'b1) begin
+            ++cnt;
+        end else begin
+            break;
+        end
+    end
+    return cnt;
+endfunction
+
+//! Calculates the bit width of each adder.
+function automatic adder_plan_t planAdders();
+    adder_plan_t plan = '{default:'0};
+    int unsigned n_adders, n_curr_summands;
+
+    // depth 0
+    n_adders = N_IN_ELEMS/2;
+    plan.n_elems[0] = n_adders + uint_t'(N_IN_ELEMS[0]);
+    for (int unsigned i=0; i<n_adders; ++i) begin
+        plan.type_mat[0][i] = 1'b1; // adder
+        plan.min_val_mat[0][i] = BW_OUT'(2)*BW_OUT'(signed'({1'b1, (BW_IN_ELEM-1)'(1'b0)}));
+        plan.bw_mat[0][i] = BW_IN_ELEM+1;
+    end
+    if (N_IN_ELEMS[0] == 1'b1) begin
+        plan.min_val_mat[0][n_adders] = BW_OUT'(signed'({1'b1, (BW_IN_ELEM-1)'(1'b0)}));
+        plan.bw_mat[0][n_adders] = BW_IN_ELEM;
+    end
+
+    // depth 1 and above
+    n_curr_summands = plan.n_elems[0];
+    for (int unsigned d=1; d<TREE_HEIGHT; ++d) begin
+        n_adders = n_curr_summands/2;
+        plan.n_elems[d] = n_adders + uint_t'(n_curr_summands[0]);
+        for (int unsigned i=0; i<n_adders; ++i) begin
+            plan.type_mat[d][i] = 1'b1; // adder
+            plan.min_val_mat[d][i] = plan.min_val_mat[d-1][2*i] + plan.min_val_mat[d-1][2*i+1];
+            plan.bw_mat[d][i] = BW_OUT - cntLdgOnes(plan.min_val_mat[d][i]) + 1;
+        end
+        if (n_curr_summands[0] == 1'b1) begin
+            plan.min_val_mat[d][n_adders] = plan.min_val_mat[d-1][2*n_adders];
+            plan.bw_mat[d][n_adders] = plan.bw_mat[d-1][2*n_adders];
+        end
+        n_curr_summands = plan.n_elems[d];
+    end
+
+    return plan;
+endfunction
+// --------------------
+
+// ---------- signals and storage ----------
+var logic [TREE_HEIGHT-1:0] r_vld_dly_line; //! delayed valid signal
+
+localparam adder_plan_t adder_plan = planAdders();
+generate
+    for (genvar d=0; d<TREE_HEIGHT; ++d) begin: gen_elem_depth
+        for (genvar i=0; i<adder_plan.n_elems[d]; ++i) begin: gen_elem_horiz
+            var logic signed [adder_plan.bw_mat[d][i]-1:0] r_elem; //! element, sum/pass-through
+        end
+    end
+endgenerate
+// --------------------
+
+// ---------- instances ----------
+// --------------------
+
+// ---------- Drives output signals. ----------
+assign o_out_vld = r_vld_dly_line[TREE_HEIGHT-1];
+assign o_sum = gen_elem_depth[TREE_HEIGHT-1].gen_elem_horiz[0].r_elem;
+// --------------------
+
+// ---------- blocks ----------
+//! Update valid delay line
+always_ff @(posedge i_clk) begin: blk_update_vld_dly_line
+    if (i_sync_rst) begin
+        r_vld_dly_line <= '0;
+    end else if (!USE_CLK_EN || i_clk_en) begin
+        r_vld_dly_line <= {r_vld_dly_line[0+:TREE_HEIGHT-1], 1'b1};
+    end
+end
+
+//! Update depth-0 elements
+generate
+    for (genvar i=0; i<adder_plan.n_elems[0]; ++i) begin: gen_update_d0
+        always_ff @(posedge i_clk) begin: blk_update_d0
+            if (i_sync_rst) begin
+                if (USE_DSP_RST) begin
+                    gen_elem_depth[0].gen_elem_horiz[i].r_elem <= '0;
+                end
+            end else if (!USE_CLK_EN || i_clk_en) begin
+                localparam int unsigned BW = adder_plan.bw_mat[0][i];
+                localparam logic elem_type = adder_plan.type_mat[0][i];
+                if (elem_type) begin
+                    gen_elem_depth[0].gen_elem_horiz[i].r_elem <= BW'(signed'(i_elems[2*i])) + BW'(signed'(i_elems[2*i+uint_t'(elem_type)]));
+                end else begin
+                    gen_elem_depth[0].gen_elem_horiz[i].r_elem <= BW'(signed'(i_elems[2*i]));
+                end
+            end
+        end
+    end
+endgenerate
+
+//! Update depth-1-and_above elements
+generate
+    for (genvar d=1; d<TREE_HEIGHT; ++d) begin: gen_update_depth
+        for (genvar i=0; i<adder_plan.n_elems[d]; ++i) begin: gen_update_horiz
+            always_ff @(posedge i_clk) begin: blk_update_dx
+                if (i_sync_rst) begin
+                    if (USE_DSP_RST) begin
+                        gen_elem_depth[d].gen_elem_horiz[i].r_elem <= '0;
+                    end
+                end else if (!USE_CLK_EN || i_clk_en) begin
+                    localparam int unsigned BW = adder_plan.bw_mat[d][i];
+                    localparam logic elem_type = adder_plan.type_mat[d][i];
+                    if (elem_type) begin
+                        //gen_elem_depth[d].gen_elem_horiz[i].r_elem <= BW'(gen_elem_depth[d-1].gen_elem_horiz[2*i].r_elem) + BW'(gen_elem_depth[d-1].gen_elem_horiz[2*i+1].r_elem); // This causes elaboration errors 'Can't find definition of 'r_elem' in dotted signal: 'gen_elem_depth__BRA__<some number>__KET__.gen_elem_horiz__BRA__<some number>__KET__.r_elem'.
+                        gen_elem_depth[d].gen_elem_horiz[i].r_elem <= BW'(gen_elem_depth[d-1].gen_elem_horiz[2*i].r_elem) + BW'(gen_elem_depth[d-1].gen_elem_horiz[2*i+elem_type].r_elem);
+                    end else begin
+                        gen_elem_depth[d].gen_elem_horiz[i].r_elem <= BW'(gen_elem_depth[d-1].gen_elem_horiz[2*i].r_elem);
+                    end
+                end
+            end
+        end
+    end
+endgenerate
+// --------------------
+endmodule
+
+`default_nettype wire
+
+`endif // TREE_ADDER_TYP_A_V0_2_0_SV_DEFINED
